956647f3128aafea79e22f77f798a2b5354cf2a2
magio-hercules <kjmhercules@gmail.com>
2024-10-02 16:31:59 +0900

    1. #9까지의 내용 2. root에서 모든 Tweets 표시하기

--- rooms/views.py
+++ rooms/views.py
@@ -1,3 +1,37 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Room
 
-# Create your views here.
+
+def see_all_rooms(request):
+    rooms = Room.objects.all()
+    return render(
+        request,
+        "all_rooms.html",
+        {
+            "rooms": rooms,
+            "title": "Hello this title comes from django!",
+        },
+    )
+
+
+def see_one_room(request, room_pk):
+    try:
+        print(f"see_one_room room_pk: {room_pk}")
+        room = Room.objects.get(pk=room_pk)
+        print(room)
+        return render(
+            request,
+            "room_detail.html",
+            {
+                "room": room,
+            },
+        )
+    except Room.DoesNotExist:
+        return render(
+            request,
+            "room_detail.html",
+            {
+                "not_found": True,
+            },
+        )
