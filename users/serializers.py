174d4a4eb89ceb375355f1ca2765e23d77585406
magio-hercules <kjmhercules@gmail.com>
2024-10-13 00:02:38 +0900

    1. #12 강의 반영 2. 졸업과제 users API 적용

--- users/serializers.py
+++ users/serializers.py
@@ -6,7 +6,24 @@ class TinyUserSerializer(ModelSerializer):
     class Meta:
         model = User
         fields = (
+            "pk",
             "name",
             "avatar",
             "username",
+        )
+
+
+class PrivateUserSerializer(ModelSerializer):
+    class Meta:
+        model = User
+        exclude = (
+            "password",
+            "is_superuser",
+            "id",
+            "is_staff",
+            "is_active",
+            "first_name",
+            "last_name",
+            "groups",
+            "user_permissions",
         )
--- users/urls.py
+++ users/urls.py
@@ -1,5 +1,15 @@
 from django.urls import path
 from . import views
 
-
-urlpatterns = []
+urlpatterns = [
+    path("me", views.Me.as_view()),
+    path("change-password", views.ChangePassword.as_view()),
+    path("@<str:username>", views.PublicUser.as_view()),
+    # 과제
+    path("", views.Users.as_view()),
+    path("<int:pk>", views.PublicUser.as_view()),
+    path("<int:pk>/tweets", views.UserTweets.as_view()),
+    path("password", views.ChangePassword.as_view()),
+    path("login", views.LogIn.as_view()),
+    path("logout", views.LogOut.as_view()),
+]
